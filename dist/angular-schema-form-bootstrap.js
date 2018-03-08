eeca9b2cb815d8aa7141fe4cef57d42d56e6e239
ronak-jain96 <36759681+ronak-jain96@users.noreply.github.com>
2018-03-08 12:53:42 +0530

    Update angular-schema-form-bootstrap.js

--- dist/angular-schema-form-bootstrap.js
+++ dist/angular-schema-form-bootstrap.js
@@ -69,7 +69,7 @@
 /******/ 	__webpack_require__.p = "";
 
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 21);
+/******/ 	return __webpack_require__(__webpack_require__.s = 22);
 /******/ })
 /************************************************************************/
 /******/ ([
